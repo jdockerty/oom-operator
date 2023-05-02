16608b469bb0cb93fc7d0ea722a17085ab4e3fa1
Jack Dockerty <jdockerty19@gmail.com>
2023-05-02 21:21:53 +0100

    initial commit

---
+++ api/v1alpha1/oomer_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2023 Jack Dockerty.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// OomerSpec defines the desired state of Oomer
+type OomerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Oomer. Edit oomer_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// OomerStatus defines the observed state of Oomer
+type OomerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Oomer is the Schema for the oomers API
+type Oomer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   OomerSpec   `json:"spec,omitempty"`
+	Status OomerStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// OomerList contains a list of Oomer
+type OomerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Oomer `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Oomer{}, &OomerList{})
+}
